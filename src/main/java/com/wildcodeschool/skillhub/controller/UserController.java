ccf3ff7d6aa758a54f884034ae68f15c3380e131
Thoralf Friedrich <thoralf.friedrich@telekom.de>
2020-02-19 15:28:58 +0100

    Prüfung Mailformat

--- src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
+++ src/main/java/com/wildcodeschool/skillhub/controller/UserController.java
@@ -4,6 +4,9 @@ import com.wildcodeschool.skillhub.entity.*;
 import com.wildcodeschool.skillhub.entity.User;
 import com.wildcodeschool.skillhub.repository.UserRepository;
 import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -19,22 +24,6 @@ public class UserController {
 
 	private UserRepository repository = new UserRepository();
 
-//	@GetMapping("/user/profile")
-//	public String viewProfile(Model model) {
-/*        User user = new User();
-        User optionalUser = new User();
-		user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		Long userId = user.getUserId();
-		if (userId != null) {
-			optionalUser = user.findById(userId);
-//			if (optionalUser.isPresent()) {
-//				user = optionalUser.get();
-//			}
-		}
-		model.addAttribute("user", user);
-		return "user/profile";
-    }
-*/
 
 	@GetMapping("/register")
 	public String getUser(Model model, @RequestParam(required = false) Long userid)
@@ -46,21 +35,34 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "register";
 	}
-
 	@PostMapping("/register")
-	public String postUser(@ModelAttribute User user, BindingResult bindingResult) 
-	{
-		if (!bindingResult.hasErrors()){
-			
-			if (user.getUserId() != null) {
-				repository.update(user);
-			} else {
-				repository.save(user);
-			}
-			return "redirect:/";
+	public String checkPersonInfo(@Valid User user, BindingResult bindingResult, Model model) {
+		// Lookup user in database by nickname
+		User userExists = repository.findByNick(user.getNickName());
+		ModelAndView modelAndView = new ModelAndView();
+
+		System.out.println(userExists);
+
+		if (userExists != null) {
+			System.out.println("if-Zweig");
+			modelAndView.addObject("NickMessage", "Oops!  There is already a user registered with the email provided.");
+			model.addAttribute("NicknameMessage", "abc");
+			modelAndView.setViewName("register");
+			bindingResult.reject("nickname");
 		}
 		else {
+			System.out.println("Else-Zweig");
+		}
+		if (bindingResult.hasErrors()) {
+			System.out.println(bindingResult.hasErrors());
 			return "register";
 		}
+		if (user.getUserId() != null) {
+			repository.update(user);
+		} else {
+			repository.save(user);
+		}
+
+		return "redirect:/";
 	}
 }
